e28ffecd6e52fe15a55dd9419ba4ed5a217a4cfc
rivera-bl <rivera.blo@protonmail.com>
2022-05-09 21:02:22 -0400

    Output privates ips

--- terraform-aws-ec2-basic/outputs.tf
+++ terraform-aws-ec2-basic/outputs.tf
@@ -12,3 +12,8 @@ output "instance_public_ip" {
   value       = aws_instance.this[*].public_ip
   description = "Public ip of the EC2 instance"
 }
+
+output "instance_private_ip" {
+  value       = aws_instance.this[*].private_ip
+  description = "Public ip of the EC2 instance"
+}
